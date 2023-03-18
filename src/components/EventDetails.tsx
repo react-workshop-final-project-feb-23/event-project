e04f8d93d8aaf268d4c134dc322b6d0c23fad649
Harshavardhan Reddy <hmalipatlolla@gmail.com>
2023-03-17 20:42:31 -0500

    service fix

--- src/components/EventDetails.tsx
+++ src/components/EventDetails.tsx
@@ -7,14 +7,14 @@ import { useParams } from 'react-router-dom';
 const EventDetails = () => {
   const [event, setEvent] = useState<any | undefined>();
   const pathId: string | undefined = useParams().id;
-  // useEffect(() => {
-  //   if (pathId) {
-  //     getEventById("G5vYZ98KcFtaq")
-  //       .then((res) => {
-  //        console.log('test res', res)
-  //       })
-  //     }
-  // }, [pathId]);
+  useEffect(() => {
+    if (pathId) {
+      getEventById("G5vYZ98KcFtaq")
+        .then((res) => {
+         console.log('test res', res)
+        })
+      }
+  }, [pathId]);
   return (
     <div className='EventDetails'>
         EventDetails works
--- src/services/getEventById.ts
+++ src/services/getEventById.ts
@@ -5,7 +5,7 @@ const api_key: string = process.env.REACT_APP_GIPHY_API_KEY || "";
 
 
 export const getEventById = (event_Id: string): Promise<any> => {
-    return axios.get(`https://app.ticketmaster.eu/mfxapi/v2/events/${event_Id}`, {
+    return axios.get(`https://app.ticketmaster.com/discovery/v2/events/${event_Id}`, {
         params: {
             apikey: api_key
         },
