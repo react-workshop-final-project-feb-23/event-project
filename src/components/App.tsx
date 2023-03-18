7d5a90ad2d5cf1d6c9582a8935ffa014dea8c63e
Harshavardhan Reddy <hmalipatlolla@gmail.com>
2023-03-18 16:14:36 -0500

    added login page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,22 +10,30 @@ import EventHeader from "./EventHeader";
 import EventMain from "./EventMain";
 import EventDetails from "./EventDetails";
 import EventResultList from "./EventResultList";
+import Auth from "./Auth";
+
+let loggedIn = JSON.parse(sessionStorage.getItem("logedIn") || "false");
+if(loggedIn === "false") {
+  loggedIn = false;
+}
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <EventHeader />
-        <Routes>
-          <Route path="/" element={<EventMain/>} />
-          <Route path="/search" element={<EventMain/>} />
-          <Route path="/event/:id" element={<EventDetails/>} />
-          {/* Wildcard - redirect - asterick*/}
-          <Route path="*" element={<Navigate to="/" />} />
-          <Route path="/favorites" element={<EventResultList/>} />
-
-        </Routes>
-      </Router>
+     {loggedIn}
+        <Router>
+        {loggedIn && <EventHeader /> }
+          <Routes>
+          {loggedIn && <Route path="/" element={<EventMain />} />}
+          {!loggedIn && <Route path="/" element={<Auth />} />}
+            {!loggedIn && <Route path="/signin" element={<Auth />} />}
+            {loggedIn && <Route path="/search" element={<EventMain />} />}
+            {loggedIn &&  <Route path="/event/:id" element={<EventDetails />} />}
+            {/* Wildcard - redirect - asterick*/}
+            {loggedIn && <Route path="*" element={<Navigate to="/signin" />} />}
+            {loggedIn &&  <Route path="/favorites" element={<EventResultList />} />}
+          </Routes>
+        </Router>
     </div>
   );
 }
--- src/components/EventHeader.tsx
+++ src/components/EventHeader.tsx
@@ -6,25 +6,55 @@ import Typography from "@mui/material/Typography";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
-import LocalActivityOutlinedIcon from '@mui/icons-material/LocalActivityOutlined';
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import LocalActivityOutlinedIcon from "@mui/icons-material/LocalActivityOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+import { useNavigate } from "react-router-dom";
 
 const EventHeader = () => {
+  const navigate = useNavigate();
+  const handleLogout = (): void => {
+    sessionStorage.setItem("logedIn", JSON.stringify(false));
+    navigate("/signin");
+    window.location.reload();
+  };
   return (
     <div className="EventHeader">
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static">
           <Toolbar>
             <Typography variant="h6" component="span" sx={{ flexGrow: 5 }}>
-            <Link href="/" underline="hover">
-            <h1><LocalActivityOutlinedIcon  style={{ color: 'black', fontSize: 60,  paddingRight: 10}} fontSize="large"/> Events</h1>
-          </Link>
+              <Link href="/" underline="hover">
+                <h1>
+                  <LocalActivityOutlinedIcon
+                    style={{ color: "black", fontSize: 60, paddingRight: 10 }}
+                    fontSize="large"
+                  />{" "}
+                  Events
+                </h1>
+              </Link>
             </Typography>
             <Typography variant="h6" component="span" sx={{ flexGrow: 1 }}>
-            <Link href="/favorites" underline="hover">
-            <h1>Favorites <FavoriteBorderOutlinedIcon  style={{ color: 'black', fontSize: 60, paddingLeft: 10}} fontSize="large"/></h1>
-          </Link>
+              <Link href="/favorites" underline="hover">
+                <h1>
+                  <FavoriteBorderOutlinedIcon
+                    style={{ color: "black", fontSize: 50, paddingRight: 10 }}
+                    fontSize="large"
+                  />
+                  Favorites
+                </h1>
+              </Link>
             </Typography>
+            <Button
+              variant="contained"
+              onClick={handleLogout}
+              color="error"
+              endIcon={<LogoutOutlinedIcon />}
+              type="submit"
+              sx={{ borderRadius: 3 }}
+            >
+              Signout
+            </Button>
           </Toolbar>
         </AppBar>
       </Box>
