17d600f8b3432ada77efb61ec1d0520cc605ff07
Joshua LeDuc <joshualeduc@outlook.com>
2023-03-20 19:57:58 -0400

    Adding folder structure to components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,41 +1,35 @@
-import React from "react";
-import "./App.css";
-import {
-  BrowserRouter as Router,
-  Navigate,
-  Route,
-  Routes,
-} from "react-router-dom";
-import EventHeader from "./EventHeader";
-import EventMain from "./EventMain";
-import EventDetails from "./EventDetails";
-import EventResultList from "./EventResultList";
-import Auth from "./Auth";
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom'
+import EventHeader from './EventHeader'
+import EventMain from './Event Main/EventMain'
+import EventDetails from './EventDetails/EventDetails'
+import EventResultList from './Event Main/EventList/EventResultList'
+import Auth from './Auth/Auth'
 
-let loggedIn = JSON.parse(sessionStorage.getItem("logedIn") || "false");
-if(loggedIn === "false") {
-  loggedIn = false;
+let loggedIn = JSON.parse(sessionStorage.getItem('logedIn') || 'false')
+if (loggedIn === 'false') {
+  loggedIn = false
 }
 
 function App() {
-
   return (
-    <div className="App">
-        <Router>
-        {loggedIn && <EventHeader /> }
-          <Routes>
-          {loggedIn && <Route path="/" element={<EventMain />} />}
-          {!loggedIn && <Route path="/" element={<Auth />} />}
-            {!loggedIn && <Route path="/signin" element={<Auth />} />}
-            {loggedIn && <Route path="/search" element={<EventMain />} />}
-            {loggedIn &&  <Route path="/event/:id" element={<EventDetails />} />}
-            {/* Wildcard - redirect - asterick*/}
-            {loggedIn && <Route path="*" element={<Navigate to="/signin" />} />}
-            {/* {loggedIn &&  <Route path="/favorites" element={<EventResultList />} />} */}
-          </Routes>
-        </Router>
+    <div className='App'>
+      <Router>
+        {loggedIn && <EventHeader />}
+        <Routes>
+          {loggedIn && <Route path='/' element={<EventMain />} />}
+          {!loggedIn && <Route path='/' element={<Auth />} />}
+          {!loggedIn && <Route path='/signin' element={<Auth />} />}
+          {loggedIn && <Route path='/search' element={<EventMain />} />}
+          {loggedIn && <Route path='/event/:id' element={<EventDetails />} />}
+          {/* Wildcard - redirect - asterick*/}
+          {loggedIn && <Route path='*' element={<Navigate to='/signin' />} />}
+          {/* {loggedIn &&  <Route path="/favorites" element={<EventResultList />} />} */}
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
