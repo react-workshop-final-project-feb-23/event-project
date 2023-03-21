24eb0c0e17893b3ccf07ca477c2f4f9626fc794b
Harshavardhan Reddy <hmalipatlolla@gmail.com>
2023-03-20 20:44:40 -0500

    main page changes

--- src/components/Event Main/EventList/EventResult.tsx
+++ src/components/Event Main/EventList/EventResult.tsx
@@ -1,14 +1,15 @@
-import './EventResult.css'
+import "./EventResult.css";
 import { Link } from "react-router-dom";
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import BookmarkAddOutlinedIcon from '@mui/icons-material/BookmarkAddOutlined';
-
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import BookmarkAddOutlinedIcon from "@mui/icons-material/BookmarkAddOutlined";
+import Grid from "@mui/material/Grid";
+import moment from 'moment'
 
 
 interface ResultProps {
@@ -17,42 +18,49 @@ interface ResultProps {
 
 const EventResult = ({ event }: ResultProps) => {
   return (
-
-    <Card sx={{ maxWidth: 345 }} style={{margin: 20, boxShadow :"5px 5px 10px #ccc" }}>
-      <>
-        <CardMedia
-          sx={{ height: 140 }}
-          image={event?._embedded.attractions[0]?.images[0].url}
-          title="green iguana"
-        />
-        {event?._embedded.attractions[1] &&
-          <CardMedia
-            sx={{ height: 140 }}
-            image={event?._embedded.attractions[1]?.images[0].url}
-            title="green iguana"
-          />}
-      </>
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {event?.name}
-        </Typography>
-        <h4>{event?._embedded.venues[0].name}</h4>
-        <h4>
-          {event?._embedded.venues[0].address.line1},
-          {event?._embedded.venues[0].city.name},
-          {event?._embedded.venues[0].state.name}, {event?._embedded.venues[0].country.countryCode},
-          {event?._embedded.venues[0].postalCode}
-        </h4>
-      </CardContent>
-      <CardActions>
-        <Link to={`/event/${event.id}`}>
-          <Button size="small">Details</Button>
-
-        </Link>
-        <Button size="small"><BookmarkAddOutlinedIcon/>Add to Favorites</Button>
-      </CardActions>
-    </Card>
-  )
+    
+        <Card style={{maxWidth: 500,  margin: '0px 0px 20px 20px'}}>
+          <>
+            <CardMedia
+              sx={{ height: 140 }}
+              image={event?._embedded.attractions[0]?.images[0].url}
+              title="green iguana"
+            />
+            {event?._embedded.attractions[1] && (
+              <CardMedia
+                sx={{ height: 140 }}
+                image={event?._embedded.attractions[1]?.images[0].url}
+                title="green iguana"
+              />
+            )}
+          </>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {event?.name}
+            </Typography>
+            <Typography gutterBottom variant="h6" component="div">
+            {moment(event?.dates?.start?.localDate, 'Y/M/D').format('dddd, MMMM D, Y')} at{' '}
+                        {moment(event?.dates?.start?.localTime, 'h/m/s').format('hh:mm A')}            </Typography>
+            <h4>{event?._embedded.venues[0].name}</h4>
+            <h4>
+              {event?._embedded.venues[0].address.line1},
+              {event?._embedded.venues[0].city.name},
+              {event?._embedded.venues[0].state.name},{" "}
+              {event?._embedded.venues[0].country.countryCode},
+              {event?._embedded.venues[0].postalCode}
+            </h4>
+          </CardContent>
+          <CardActions >
+            <Link to={`/event/${event.id}`} style={{paddingRight: 20}}>
+              <Button size="small">Details</Button>
+            </Link>
+            <Button size="small">
+              <BookmarkAddOutlinedIcon />
+              Add to Favorites
+            </Button>
+          </CardActions>
+        </Card>
+  );
 };
 
 export default EventResult;
--- src/components/Event Main/EventList/EventResultList.tsx
+++ src/components/Event Main/EventList/EventResultList.tsx
@@ -1,26 +1,30 @@
-import { useContext } from 'react'
-import EventResult from './EventResult'
-import './EventResultList.css'
+import { useContext } from "react";
+import EventResult from "./EventResult";
+import "./EventResultList.css";
 
-import { BucketListContext } from '../../../context/BucketListContext'
+import { BucketListContext } from "../../../context/BucketListContext";
+import Grid from "@mui/material/Grid";
+import { Box } from "@mui/material";
 
 interface ResultListProps {
-  events: any
+  events: any;
 }
 
 const EventResultList = ({ events }: ResultListProps) => {
-  console.log('test', events)
-  const { bucketList } = useContext(BucketListContext)
+  console.log("test", events);
+  const { bucketList } = useContext(BucketListContext);
   return (
-    <div className='ResultsList'>
+    <div className="ResultsList">
       <h2>Results</h2>
-      <ul>
-        {events?._embedded?.events?.map((item: any) => (
-          <EventResult key={`${item.id} ${Math.random()}`} event={item} />
-        ))}
-      </ul>
+      <Grid container  style={{ display: 'grid' }} >
+        <Grid item >
+          {events?._embedded?.events?.map((item: any) => (
+            <EventResult key={`${item.id} ${Math.random()}`} event={item} />
+          ))}
+        </Grid>
+      </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default EventResultList
+export default EventResultList;
